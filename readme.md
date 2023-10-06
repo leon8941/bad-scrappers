a7d60b3a3d79b1148e17538f6d08b41eccdc1aae
leon8941 <leon8941@gmail.com>
2023-10-06 16:13:20 +0800

    wip

--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
+### With Bun 
 1. Use bun.js https://bun.sh/.
 2. After install bun, run `exec /bin/zsh`.
 3. After bun is available in CLI, run `bun install` to install packages.
 4. Hurray
+
+### How to get around with anti-bots? :robot:
+
+Bot detection website 
+
+1. https://bot.sannysoft.com/
+2. https://bot.incolumitas.com/
--- sample1.js
+++ sample1.js
@@ -4,7 +4,9 @@ import puppeteer from 'puppeteer';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto('https://shopee.com.my/');
+  await page.goto('https://shopee.com.my/cadbury_oreo_officialstore#product_list');
+  //GET all product list - https://shopee.com.my/api/v4/shop/rcmd_items?bundle=shop_page_category_tab_main&limit=30&offset=60&shop_id=59165532&sort_type=1&upstream=
+
 
   const url = await page.url();
   console.log(url);
